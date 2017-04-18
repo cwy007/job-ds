bd06e9f046567ec2216d0bb8b2abe6dde2150b0d
chanweiyan <chanweiyan007@gmail.com>
2017-04-18 10:14:11 +0800

    install font-awesome-rails & apply to job-status

--- app/helpers/jobs_helper.rb
+++ app/helpers/jobs_helper.rb
@@ -1,9 +1,11 @@
 module JobsHelper
   def render_job_status(job)
     if job.is_hidden
-      "(Hidden)"
+      content_tag(:span, " ", class: "fa fa-lock fa-lg")
+
     else
-      "(Public)"
+      content_tag(:span, " ", class: "fa fa-unlock fa-2x")
+
     end
   end
 end
