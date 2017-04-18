d293170912f0b2de5f4a64bd9e08031e9dea6744
chanweiyan <chanweiyan007@gmail.com>
2017-04-18 11:26:00 +0800

    add resume model and hook job/user relationship

--- app/models/job.rb
+++ app/models/job.rb
@@ -4,8 +4,13 @@ class Job < ApplicationRecord
   validates :wage_lower_bound, presence: true
   validates :wage_lower_bound, numericality: { greater_than: 0 }
 
+  has_many :resumes
+  belongs_to :user
+
   scope :recent, -> { order('created_at DESC')}
   scope :publish, -> { where(:is_hidden => false) }
+
+  
   def publish!
     self.is_hidden = false
     self.save
