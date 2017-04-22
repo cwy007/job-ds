6731599efefc1d7049a4812ffbbb96d3cce6ba3b
chanweiyan <chanweiyan007@gmail.com>
2017-04-22 10:00:55 +0800

    edit seed 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,19 +12,19 @@ create_account = User.create([email: 'example@gmail.com', password: '12345678', 
 puts "Admin account created."
 
 create_jobs = for i in 1..10 do
-  Job.create!([title: "Java高级工程师", description: "1、负责互联网产品后端服务器架构设计，要求系统具有可扩展性， 能够支持大量并发用户；
-  2、根据产品需求完成服务器架构和模块设计、编码、测试以及文档编写等工作；
-  3、优化系统性能，保证服务器质量和性能。", wage_upper_bound: 30000, wage_lower_bound: 20000, is_hidden: "false"])
+  # Job.create!([title: "Java高级工程师", description: "1、负责互联网产品后端服务器架构设计，要求系统具有可扩展性， 能够支持大量并发用户；
+  # 2、根据产品需求完成服务器架构和模块设计、编码、测试以及文档编写等工作；
+  # 3、优化系统性能，保证服务器质量和性能。", wage_upper_bound: 30000, wage_lower_bound: 20000, is_hidden: "false"])
 
-  # Job.create!([title: "Job no.#{i}", description: "這是用種子建立的第 #{i} 個Public工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100, is_hidden: "false"])
+  Job.create!([title: "Job no.#{i}", description: "這是用種子建立的第 #{i} 個Public工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100, is_hidden: "false"])
 end
 puts "10 Public jobs created."
 
 create_jobs = for i in 1..10 do
-  Job.create!([title: "Key Account Manager", description: "1. Job purpose Enhance CB&GB team in Shanghai, and build strong relationship with customers.
-  2. Key challenges (in first 6 – 12m) Know how to operate the REF business.
-  3. It’s much different from tire and engine oil.Core Responsibilities BD new CB&GB customers.", wage_upper_bound: 50000, wage_lower_bound: 10000, is_hidden: "false"])
+  # Job.create!([title: "Key Account Manager", description: "1. Job purpose Enhance CB&GB team in Shanghai, and build strong relationship with customers.
+  # 2. Key challenges (in first 6 – 12m) Know how to operate the REF business.
+  # 3. It’s much different from tire and engine oil.Core Responsibilities BD new CB&GB customers.", wage_upper_bound: 50000, wage_lower_bound: 10000, is_hidden: "false"])
 
-  # Job.create!([title: "Job no.#{i+10}", description: "這是用種子建立的第 #{i+10} 個Hidden工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100,is_hidden: "true"])
+  Job.create!([title: "Job no.#{i+10}", description: "這是用種子建立的第 #{i+10} 個Hidden工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100,is_hidden: "true"])
 end
 puts "10 Hidden jobs created."
