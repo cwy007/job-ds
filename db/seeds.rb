6d75441d7343372cf12d35fc532394ea5a5b350b
chanweiyan <chanweiyan007@gmail.com>
2017-04-23 20:35:52 +0800

    edit seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,17 +13,20 @@ puts "Admin account created."
 
 create_jobs = for i in 1..10 do
 
-  job = Job.create!([title: "Job no.#{i}", description: "這是用種子建立的第 #{i} 個Public工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100, is_hidden: "false", user_id: 1])
+  job = Job.create!([title: "Job no.#{i}", description: "這是用種子建立的第 #{i} 個Public工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100, is_hidden: "false", user_id: 1, category_id: 1])
 
 end
 puts "10 Public jobs created."
 
 create_jobs = for i in 1..10 do
 
-  Job.create!([title: "Job no.#{i+10}", description: "這是用種子建立的第 #{i+10} 個Hidden工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100,is_hidden: "true", user_id: 1])
+  Job.create!([title: "Job no.#{i+10}", description: "這是用種子建立的第 #{i+10} 個Hidden工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100,is_hidden: "true", user_id: 1, category_id: 2])
 end
 puts "10 Hidden jobs created."
-
+["Ruby", "Java", "Node.js", "PHP", "Python", "Go"].each do |c|
+  category = Category.create(name: c)
+end
+puts "6 categories created."
 
 
 # Job.create!([title: "Java高级工程师", description: "1、负责互联网产品后端服务器架构设计，要求系统具有可扩展性， 能够支持大量并发用户；
