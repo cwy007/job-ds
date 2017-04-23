8eaec2a5aaecd39bd6b65d96f28bd7efcc2a7833
chanweiyan <chanweiyan007@gmail.com>
2017-04-23 21:04:24 +0800

    change order

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,11 @@ puts "這個種子檔會自動建立一個admin帳號, 並且創建 10 個 publi
 create_account = User.create([email: 'example@gmail.com', password: '12345678', password_confirmation: '12345678', is_admin: 'true'])
 puts "Admin account created."
 
+["Ruby", "Java", "Node.js", "PHP", "Python", "Go"].each do |c|
+  category = Category.create(name: c)
+end
+puts "6 categories created."
+
 create_jobs = for i in 1..10 do
 
   job = Job.create!([title: "Job no.#{i}", description: "這是用種子建立的第 #{i} 個Public工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100, is_hidden: "false", user_id: 1, category_id: 1])
@@ -23,10 +28,7 @@ create_jobs = for i in 1..10 do
   Job.create!([title: "Job no.#{i+10}", description: "這是用種子建立的第 #{i+10} 個Hidden工作", wage_upper_bound: rand(50..99)*100, wage_lower_bound: rand(10..49)*100,is_hidden: "true", user_id: 1, category_id: 2])
 end
 puts "10 Hidden jobs created."
-["Ruby", "Java", "Node.js", "PHP", "Python", "Go"].each do |c|
-  category = Category.create(name: c)
-end
-puts "6 categories created."
+
 
 
 # Job.create!([title: "Java高级工程师", description: "1、负责互联网产品后端服务器架构设计，要求系统具有可扩展性， 能够支持大量并发用户；
